3352cbf091436c735c92b9dd7e40da5900e316b2
shepik <shepik@yandex.ru>
2011-05-22 00:30:41 +0400

    pipe done

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-INCLUDES=-I/usr/include/webkit-1.0/ -I/usr/include/libsoup-2.4/ -I/usr/include/gtk-3.0/ -I/usr/include/glib-2.0/ -I/usr/lib/glib-2.0/include/ -I/usr/include/pango-1.0/ -I/usr/include/cairo/ -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/atk-1.0
+INCLUDE=-I/usr/include/webkit-1.0/ -I/usr/include/libsoup-2.4/ -I/usr/include/gtk-3.0/ -I/usr/include/glib-2.0/ -I/usr/lib/glib-2.0/include/ -I/usr/include/pango-1.0/ -I/usr/include/cairo/ -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/atk-1.0 -I/usr/include/gio-unix-2.0/
 LIBS=-lwebkit-1.0
-main: main.cpp
-	g++ -o main -O0 -ggdb main.cpp ${LIBS} ${INCLUDES}
+CXXFLAGS=${LIBS} ${INCLUDE} -O0 -ggdb
+
+all: main.o cJSON.o
+	g++ -o main main.o cJSON.o ${CXXFLAGS}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <assert.h>
+			
 #include <iostream>
+#include <gio/gio.h>
+#include <gio/gunixinputstream.h>
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
 #include <JavaScriptCore/JSObjectRef.h>
 #include <JavaScriptCore/JSContextRef.h>
 #include <JavaScriptCore/JSStringRef.h>
 
-static void destroy_cb(GtkWidget* widget, gpointer data) {
-	gtk_main_quit();
-}
+WebKitWebView* webView = NULL;
 
 static void load_status_cb(GObject* object, GParamSpec* pspec, gpointer data) {
 	WebKitWebView *web_view;
@@ -40,51 +46,27 @@ static void load_status_cb(GObject* object, GParamSpec* pspec, gpointer data) {
 	}
 }
 
+int pipeFdIn[2];
+int pipeFdOut[2];
+
 static JSValueRef sendToInput(JSContextRef ctx, JSObjectRef /*function*/, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception) {
-	std::cout << "Bar Called\n";
-	
+	assert(argumentCount==1);
+//	std::cout << "Bar Called\n";
+	JSValueRef ex;
+	double num = JSValueToNumber(ctx,arguments[0],&ex);
+	char c = (char)num;
+	if (c=='\r') c = '\n';
+	write(pipeFdIn[1], &c, 1);
+	std::cout << "writing.." << c << std::endl;
+/*	
+	JSStringRef str = JSValueToStringCopy(ctx, arguments[0], &ex);
+	char buf[4096];
+	JSStringGetUTF8CString(str, buf,4096);
+	std::cout << "writing.." << buf << std::endl;
+	write(pipeFdIn[1], buf, strlen(buf));
+*/	
 	return JSValueMakeUndefined(ctx);
 }
-static JSStaticFunction sFooStaticFunctions[] = {
-	{ "sendToInput",  sendToInput, kJSPropertyAttributeReadOnly },
-	{ 0, 0, 0 }
-};
-static void myclass_init_cb(JSContextRef ctx, JSObjectRef object)
-{
-// ...
-}
-
-static void myclass_finalize_cb(JSObjectRef object)
-{
-// ...
-}
-JSClassDefinition fooDef = {
-	0, kJSClassAttributeNone, "foo", 0, 
-	0, // currently no foo properties... 
-	sFooStaticFunctions,
-	myclass_init_cb,
-	myclass_finalize_cb,
-	0, // has Property
-	0, // get Property 
-	0, // set Property callback...
-	0, 0, 0, 0, 0, 0
-};
-
-
-WebKitWebView* webView = NULL;
-static void load_finished_cb(WebKitWebView *web_view, WebKitWebFrame *web_frame, gpointer data) {
-	printf("Finished downloading %s\n", webkit_web_view_get_uri(web_view));
-	webView = web_view;
-
-//	webkit_web_view_execute_script(webView, "alert(123)");
-	
-}
-
-//scriptCtx = toRef(coreFrame->script()->globalObject()->globalExec());
-//JSObjectRef global = JSContextGetGlobalObject(scriptCtx);
-//JSClassRef fooClass = JSClassCreate(&fooDef);
-//JSObjectRef  fooScriptObject = JSObjectMake(scriptCtx, fooClass, NULL);
-//JSObjectSetProperty(scriptCtx, global, JSStringRef("foo"), fooScriptObject, kJSPropertyAttributeNone, 0);
 
 static gboolean idle_func (gpointer data) {
 	if (!webView) return true;
@@ -92,25 +74,7 @@ static gboolean idle_func (gpointer data) {
 	return true;
 }
 
-static void window_object_cleared_cb(WebKitWebView  *web_view,
-                                WebKitWebFrame *web_frame,
-                                gpointer        context,
-                                gpointer        arg3,
-                                gpointer        user_data)
-
-{
-//Frame* coreFrame;
-//coreFrame = core(web_frame);
-//WebKitWebFrame *coreFrame = webView->;
-/*
-JSGlobalContextRef jsContext = webkit_web_frame_get_global_context(web_frame);
-JSObjectRef globalObj = JSContextGetGlobalObject(jsContext);
-JSClassRef classDef = JSClassCreate(&fooDef);
-JSObjectRef classObj = JSObjectMake(jsContext, classDef, context);
-JSStringRef str = JSStringRef("foo");
-JSObjectSetProperty(jsContext, globalObj, str, classObj, kJSPropertyAttributeNone, NULL);
-
-	webView = NULL;*/
+static void window_object_cleared_cb(WebKitWebView *web_view, WebKitWebFrame *web_frame, gpointer context, gpointer arg3, gpointer user_data) {
 
 	JSGlobalContextRef jsContext = webkit_web_frame_get_global_context(web_frame);
 	JSObjectRef globalObj = JSContextGetGlobalObject(jsContext);
@@ -118,10 +82,89 @@ JSObjectSetProperty(jsContext, globalObj, str, classObj, kJSPropertyAttributeNon
 	JSValueRef exception=0;
 	JSValueRef jsfoo=JSObjectMakeFunctionWithCallback(jsContext,JSStringCreateWithUTF8CString("sendToInput"),sendToInput);
 	JSObjectSetProperty(jsContext, globalObj,JSStringCreateWithUTF8CString("sendToInput"), jsfoo,kJSPropertyAttributeDontDelete | kJSPropertyAttributeReadOnly, &exception);
-
-//	return true;
 }
 
+#include "cJSON.h"
+
+char bufInput[4096];
+
+static void onPipeRead(GObject *source, GAsyncResult *res, gpointer user_data) {
+	GInputStream *inputStream = G_INPUT_STREAM (source);
+	GError *err = NULL;
+	int main_len = g_input_stream_read_finish (inputStream, res, &err);
+	bufInput[main_len] = 0;
+//	size_t size = (size_t)g_async_result_get_user_data(res);
+//	std::cout << size << std::endl;
+	if (main_len>0) {
+		for (int i=0;i<main_len;i++) std::cout << (int)bufInput[i] << ", ";
+		std::cout << std::endl;
+		
+		cJSON *json = cJSON_CreateString(bufInput);
+		char *bufJson = cJSON_Print(json);
+		char buf[strlen(bufJson)+100];
+		
+		sprintf(buf,"print(%s);",bufJson);
+		std::cout << buf << std::endl;
+		free(bufJson);
+		webkit_web_view_execute_script(webView, buf);
+	}
+	g_input_stream_read_async(inputStream, bufInput, 4096, G_PRIORITY_DEFAULT, NULL, onPipeRead,NULL);
+	
+}
+
+void run_shell() {
+	if (pipe(pipeFdIn) == -1) { perror("pipe"); exit(EXIT_FAILURE); }
+	if (pipe(pipeFdOut) == -1) { perror("pipe"); exit(EXIT_FAILURE); }
+
+	printf("pipeFdIn: %d, %d\n",pipeFdIn[0],pipeFdIn[1]);
+	printf("pipeFdOut: %d, %d\n",pipeFdOut[0],pipeFdOut[1]);
+
+	char buf;
+	pid_t cpid = fork();
+	if (cpid == -1) { perror("fork"); exit(EXIT_FAILURE); }
+	if (cpid == 0) { /* Child writes argv[1] to pipe */
+		//close(pipeFd[0]); /* Close unused read end */
+		close(pipeFdIn[1]);
+		close(pipeFdOut[0]);
+		dup2(pipeFdIn[0],0);
+		dup2(pipeFdOut[1],1);
+		close(pipeFdIn[0]);
+		close(pipeFdOut[1]);
+		//std::cout << "hi!!!!!";
+		execlp("/bin/bash","/bin/bash","-i","-l",NULL);
+		//execlp("/bin/cat","/bin/cat",NULL);
+		//write(pipeFd[1], "hi there", 8);
+		//close(pipeFd[1]); /* Reader will see EOF */
+		std::cout << "fail";
+		exit(EXIT_FAILURE);
+	} else { /* Parent reads from pipe */
+		close(pipeFdIn[0]); /* Close unused write end */
+		close(pipeFdOut[1]); /* Close unused write end */
+		//while (read(STDIN_FILENO, &buf, 1) > 0) write(pipeFd[0],&buf,1);
+		//write(pipeFdIn[0],"ASDF\n",5);close(pipeFdIn[0]);
+		//while (read(pipeFdOut[1], &buf, 1) > 0) write(STDOUT_FILENO,&buf,1);
+		
+		GInputStream * inputStream = g_unix_input_stream_new(pipeFdOut[0],false);
+		g_input_stream_read_async(inputStream, bufInput, 4096, G_PRIORITY_DEFAULT, NULL, onPipeRead,NULL);
+		/*while (read(pipeFd[0], &buf, 1) > 0)
+		write(STDOUT_FILENO, &buf, 1);
+		write(STDOUT_FILENO, "\n", 1);
+		close(pipeFd[0]);
+		wait(NULL); // Wait for child 
+		_exit(EXIT_SUCCESS);*/
+	}
+
+}
+
+static void load_finished_cb(WebKitWebView *web_view, WebKitWebFrame *web_frame, gpointer data) {
+	printf("Finished downloading %s\n", webkit_web_view_get_uri(web_view));
+	webView = web_view;
+	run_shell();
+}
+
+static void destroy_cb(GtkWidget* widget, gpointer data) {
+	gtk_main_quit();
+}
 
 int main(int argc, char* argv[]) {
 	const char *uri;
@@ -160,6 +203,7 @@ int main(int argc, char* argv[]) {
 	gtk_widget_show_all(window);
 	
 	g_idle_add (idle_func, NULL);
+	
 	gtk_main();
 	return 0;
 }
